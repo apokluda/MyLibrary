9f4cf0478acfcd79ae85e404cc897dc86ba02724
Alexander Pokluda <apokluda@gmail.com>
2019-02-04 02:27:25 -0500

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
+echo "Runing apt update"
 sudo apt update
+
+echo "Installing python-pip"
 sudo apt install python-pip
+
+echo "Installing virtualenv"
 pip install virtualenv
+
+echo "Creating virtualenv in directory .venv"
 virtualenv -p /usr/bin/python3 .venv
+
+echo "Activating virtualenv"
 source .venv/bin/activate
-pip install falcon peewee jsonschema pytest falcon-auth
-pip install gunicorn httpie
+
+echo "Installing dependencies..."
+pip install falcon
+pip install peewee
+pip install jsonschema
+pip install pytest
+pip install falcon-auth
+pip install gunicorn
+pip install httpie
